e59a20add0f3a50737b0147cb2283ecd7abbd7ba
Okarin-K <rachel9029@icloud.com>
2023-07-15 20:07:35 +0900

    feat: Web要約機能

---
+++ src/pages/webSummarizer.py
@@ -0,0 +1,122 @@
+from dotenv import load_dotenv
+import requests
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (SystemMessage, HumanMessage, AIMessage)
+from langchain.callbacks import get_openai_callback
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+load_dotenv()
+
+def init_pages():
+    st.set_page_config(
+        page_title="Web Summarizer",
+        page_icon="🤗"
+    )
+    st.header("Web Summarizer 🤗")
+
+def select_model():
+    model = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
+    if model == "GPT-3.5":
+        model_name = "gpt-3.5-turbo-0613"
+    else:
+        model_name = "gpt-4"
+
+    temperature = st.sidebar.slider("Temperature:", min_value=0.0, max_value=2.0, value=0.0, step=0.01)
+
+    return ChatOpenAI(temperature=temperature, model_name=model_name) 
+
+def init_messages():
+    clear_button = st.sidebar.button("Clear Conversation", key="clear")
+    if clear_button or "messages" not in st.session_state:
+        st.session_state.messages = [
+            SystemMessage(content="You are a helpful assistant.")
+        ]
+        st.session_state.costs = []
+
+def get_url_input():
+    url = st.text_input("URL: ", key="input")
+    return url
+
+def validate_url(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
+def get_content(url):
+    try:
+        with st.spinner("Fetching Content ..."):
+            response = requests.get(url)
+            response.encoding = 'urf-8'
+            soup = BeautifulSoup(response.text, 'html.parser')
+            if soup.main:
+                return soup.main.get_text()
+            elif soup.article:
+                return soup.article.get_text()
+            else:
+                return soup.body.get_text()
+    except:
+        st.write("something wrong")
+        return None
+
+def build_prompt(content, n_chars=300):
+    return f"""以下はとあるWebページのコンテンツである。内容を{n_chars}程度でわかりやすく要約してください。
+
+========
+
+{content[:1000]}
+
+========
+
+日本語で書いてください！
+"""
+
+def get_answer(llm, messages):
+    with get_openai_callback() as cb:
+        answer = llm(messages)
+    return answer.content, cb.total_cost
+
+def main():
+    init_pages()
+    llm = select_model()
+    init_messages()
+
+    container = st.container()
+    response_container = st.container()
+
+    with container:
+        url = get_url_input()
+        is_valid_url = validate_url(url)
+        if not is_valid_url:
+            st.write("Please input valud url")
+            answer = None
+        else:
+            content = get_content(url)
+            if content:
+                prompt = build_prompt(content)
+                st.session_state.messages.append(HumanMessage(content=prompt))
+                with st.spinner("ChatGPT is typing ..."):
+                    answer, cost = get_answer(llm, st.session_state.messages)
+                st.session_state.costs.append(cost)
+            else:
+                answer = None
+    
+    if answer:
+        with response_container:
+            st.markdown("## Summary")
+            st.write(answer)
+            st.markdown("---")
+            st.markdown("## Original Text")
+            st.write(content)
+    
+    costs = st.session_state.get("costs", [])
+    st.sidebar.markdown("## Costs")
+    st.sidebar.markdown(f"**Total cost: ${sum(costs):.5f}**")
+    for cost in costs:
+        st.sidebar.markdown(f"- ${cost:.5f}")
+
+if __name__ == '__main__':
+    main()

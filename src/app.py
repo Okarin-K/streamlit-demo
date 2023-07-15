1a9eb128f1279be17ad420ff7e3086999d842c84
Okarin-K <rachel9029@icloud.com>
2023-07-15 17:22:17 +0900

    feat: サイドバーの機能を追加
    - modelの選択
    - temperatureの値をスライダーで変更

--- src/app.py
+++ src/app.py
@@ -6,20 +6,36 @@ from langchain.schema import (SystemMessage, HumanMessage, AIMessage)
 
 load_dotenv()
 
-def main():
-    llm = ChatOpenAI(temperature=0)
-
+def init_pages():
     st.set_page_config(
         page_title="My Great ChatGPT",
         page_icon="🤗"
     )
     st.header("My Great ChatGPT 🤗")
 
-    # チャット履歴の初期化
-    if "messages" not in st.session_state:
+def select_model():
+    model = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
+    if model == "GPT-3.5":
+        model_name = "gpt-3.5-turbo-0613"
+    else:
+        model_name = "gpt-4"
+
+    temperature = st.sidebar.slider("Temperature:", min_value=0.0, max_value=2.0, value=0.0, step=0.01)
+
+    return ChatOpenAI(temperature=temperature, model_name=model_name) 
+
+def init_messages():
+    clear_button = st.sidebar.button("Clear Conversation", key="clear")
+    if clear_button or "messages" not in st.session_state:
         st.session_state.messages = [
             SystemMessage(content="You are a helpful assistant.")
         ]
+        st.session_state.costs = []
+
+def main():
+    init_pages()
+    llm = select_model()
+    init_messages()
 
     # ユーザーの入力を監視
     if user_input := st.chat_input("聞きたいことを入力してね！"):
